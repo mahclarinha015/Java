01b387005e7f59fdc15d2c5e255ac2a96f670eff
mahclariha015 <mariaclara.gracianopereira1A@gmail.com>
2025-04-02 08:55:02 -0300

    exercicio 14 faça 10 vezez a mensagem

---
+++ ex14.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+14. Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex14
+{
+	public static void main(String[] args) {
+	  //Declara variáveis
+        for (int i = 0; i < 10; i++)
+        System.out.println("Eu gosto de estudar algoritimos!");
+   }
+	}
+
+    
+	
+    
+
