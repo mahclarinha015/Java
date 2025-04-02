dd25e8d49acd34e6934f78260b31096ba95a695a
mahclariha015 <mariaclara.gracianopereira1A@gmail.com>
2025-04-02 09:49:19 -0300

    soma do numero 1 a 15

---
+++ ex15.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+15) Escreva um programa que calcule a soma dos números de 1 a 15.
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex15
+{
+	public static void main(String[] args) {
+	  //Declara variáveis
+        for (int i = 1; i < 15; i++);
+        soma + = i;
+        
+        
+        System.out.println("A soma dos números de 1 a 15 é: " += soma);
+   }
+	}
+
+    
+	
+    
+
