a7f62ffdc79a79b527fe4a4ae8792bc9c9760866
mahclarinha015 <mariaclara.gracianopereira1A@gmail.com>
2025-04-07 13:41:21 -0300

    Exercicio 32 correto

---
+++ Exercicio32.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+32-   Faça um programa que leia 5 números e informe o maior número.
+*******************************************************************************/
+import java.util.Scanner;
+public class Exercicio32
+{
+	public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+
+        int contador = 1; 
+        int maiorNumero = Integer.MIN_VALUE; 
+
+        while (contador <= 5) {
+            System.out.print("Digite o " + contador + "º número: ");
+            int numero = scanner.nextInt();
+
+            if (numero > maiorNumero) {
+                maiorNumero = numero;
+            }
+
+            contador++; 
+        }
+
+        System.out.println("O maior número digitado foi: " + maiorNumero);
+        
+        scanner.close(); 
+     }
+	}
+
+	
+    
+
+
+
+	
+
