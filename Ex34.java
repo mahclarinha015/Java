8c4497001b82cb144e80b12c30a57fbc1860fc70
mahclarinha015 <mariaclara.gracianopereira1A@gmail.com>
2025-04-07 15:22:21 -0300

    exercicio 34

---
+++ Ex34.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+exercicio 34
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex34
+{
+
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		do {
+		    System.out.println(contador);
+		    contador++;
+		}while (contador <= 5);
+	}
+}
