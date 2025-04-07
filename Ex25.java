4454896cb11229db93e936d64188cc676848e8b4
mahclarinha015 <mariaclara.gracianopereira1A@gmail.com>
2025-04-07 10:59:46 -0300

    exercicio 25

---
+++ Ex25.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+25- Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex25
+{
+	public static void main(String[] args) {
+		int i = 0;
+		
+		
+		while(i <= 100){
+		
+		System.out.println(i);
+		   i++;
+		}
+
+	}
+}
